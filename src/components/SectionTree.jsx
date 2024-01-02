3e7a1642b9da4a8544686790575f724fccb91b0d
Jeremie-mengi <jeremiemengi248@gmail.com>
2024-01-02 18:34:38 +0100

    FirstVersion-style

---
+++ src/components/SectionTree.jsx
@@ -0,0 +1,25 @@
+
+function SectionTree(){
+return(
+
+<section>
+<h1>Blog</h1>
+<div className="blogs">
+    <div  className="blog1">
+<img src="" alt="" className="image1" /> 
+<h2>Blog Title Goes Here</h2>
+<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration 
+    in some form, by injected humour, or randomised </p>
+    </div>
+    <div  className="blog2">
+<img src="" alt="" className="image2"/>
+<h2>Blog Title Goes Here </h2>
+<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration 
+    in some form, by injected humour, or randomised </p>
+    </div>
+</div>
+</section>
+)
+
+}
+export default SectionTree;
