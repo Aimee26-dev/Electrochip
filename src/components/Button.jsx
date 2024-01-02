76cdc0cb95e67f5ce901917f60f9b81c8ced603f
aimee aimee <Biyenga26@gmail.com>
2024-01-02 18:03:40 +0100

    refactor

---
+++ src/components/Button.jsx
@@ -0,0 +1,8 @@
+function Button(){
+    return(
+        <div>
+            <button className="button">Contact Us</button>
+        </div>
+    )
+}
+export default Button;
---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import logo from '../assets/images/logo.png'
+
+function Logo(){
+    return(
+        <div>
+        <a href="#"><img src={logo} alt="image logo" /></a>
+        <a href="#">Electrochip</a>
+        </div>
+    )
+}
+
+export default Logo;
