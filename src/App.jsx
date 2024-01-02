e3d0c31192af6822dca51fa3a2d5de3057eeaab7
Théodorebinda <theodorebinda@gmail.com>
2024-01-02 18:19:54 +0100

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import About from './components/About'
 
 function App() {
 
   return (
     <>
-
+      <About  />
     </>
   )
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,19 @@
 function About(){
     return (
-        <>
+        <div className="contenair-about">
+            <div className="description-about">
+                <h1>About Us</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum esse reprehenderit iure a ipsam officiis laboriosam ipsa, doloremque eius numquam labore ex officia harum porro fugit dolorum! Dicta, incidunt ducimus.</p>
+                <button>
+                    Read More
+                </button>
 
-        </>
+            </div>
+            <div className="image-about">
+                <img src="" alt="" />
+                <img src="" alt="" />
+            </div>
+        </div>
     )
 }
 export default About;
