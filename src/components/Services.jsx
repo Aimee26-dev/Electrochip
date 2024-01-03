1f09603bbed53c3efcf9367c892e4f611f2370a9
Joel-Lavia <joeldisidi8@gmail.com>
2024-01-03 17:07:57 +0100

    intehration du buttom read more

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,42 +1,62 @@
-import plugs from '../assets/images/plug.png'
-import s1 from '../assets/images/s1.png'
-import s2 from '../assets/images/s2.png'
-import s3 from '../assets/images/s3.png'
-import s4 from '../assets/images/s4.png'
-import s5 from '../assets/images/s5.png'
-
+import plugs from "../assets/images/plug.png";
+import s1 from "../assets/images/s1.png";
+import s2 from "../assets/images/s2.png";
+import s3 from "../assets/images/s3.png";
+import s4 from "../assets/images/s4.png";
+import s5 from "../assets/images/s5.png";
+import "./Styles/services.css";
 
 function Services() {
- return(
+  return (
     <section>
-      <h2>Our Services <img src={plugs} alt="" /> </h2>
-      <div>
-         <div><img src={s1} alt="" /> <h5>Equipment installation</h5>
-         <p>There are many variations of passages of Lorem Ipsum available,</p>
-         </div>
+      <h2 className="sercicesText">
+        Our Services<img src={plugs} alt="" />
+      </h2>
 
-         <div><img src={s2} alt="" />
-         <h5>Windmill Energy</h5>
-         <p>There are many variations of passages of Lorem Ipsum available,</p>
-         </div>
+      <div className="services">
 
-         <div> <img src={s3} alt="" />
- <h5>Equipment Maintenance</h5>
- <p>There are many variations of passages of Lorem Ipsum available,</p>
-         </div>
-<div><img src={s4} alt="" />
-<h5>Offshore Engineering</h5>
-<p>There are many variations of passages of Lorem Ipsum available,</p>
-</div>
+        <div className="service">
+        <img src={s1} alt="" /> 
 
-<div> <img src={s5} alt="" />
-<h5>Electrical Wiring</h5>
-<p>There are many variations of passages of Lorem Ipsum available,</p>
-</div>
-<button>Read More</button>
+         <div className="detail">
+          <h5>Equipment installation</h5>
+          <p className="sercicesText">There are many variations of <br /> passages of Lorem Ipsum available,</p>
+         </div>
+        </div>
+
+        <div className="service">
+          <img src={s2} alt="" />
+          <h5 >Windmill Energy</h5>
+          <p className="sercicesText">There are many variations of <br />passages of Lorem Ipsum available,</p>
+        </div>
+
+        <div className="service">
+          <img src={s3} alt="" />
+          <h5>Equipment Maintenance</h5>
+          <p className="sercicesText">There are many variations of <br />passages of Lorem Ipsum available,</p>
+        </div>
 
       </div>
+
+      <div className="services">
+
+      <div className="service">
+          <img src={s4} alt="" />
+          <h5>Offshore Engineering</h5>
+          <p className="sercicesText">There are many variations of <br /> passages of Lorem Ipsum available,</p>
+        </div>
+
+        <div className="service">
+          <img src={s5} alt="" />
+          <h5>Electrical Wiring</h5>
+          <p className="sercicesText">There are many variations of <br /> passages of Lorem Ipsum available,</p>
+        </div>
+      
+      </div>
+
+      <button>Read More</button>
+
     </section>
- )   
+  );
 }
-export default Services
+export default Services;
